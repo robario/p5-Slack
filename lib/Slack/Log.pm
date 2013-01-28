a9210689d9e3bb26e916b63ea9eddbab9f5f9a58
robario <webmaster@robario.com>
2013-01-28 07:56:48 +0000

    Fix it wraps multiply when imported more than once

--- lib/Slack/Log.pm
+++ lib/Slack/Log.pm
@@ -4,37 +4,45 @@ use warnings;
 use encoding::warnings;
 use re qw(/msx);
 
-use Encode qw(encode_utf8);
-use Text::Balanced qw(extract_quotelike);
-
-sub import {
+BEGIN {
     eval { require Smart::Comments; } or return;
 
     no warnings qw(redefine);    ## no critic (TestingAndDebugging::ProhibitNoWarnings)
 
-    my $dumper = Smart::Comments->can('Dumper');
+    my $dumper = \&Smart::Comments::Dumper;
     *Smart::Comments::Dumper = sub {
         my $dumped = $dumper->(@_);
+
+        # make unicode characters visible
         $dumped =~ s/\\x{([\dA-F]+)}/chr hex sprintf '%04s', $1/egi;
+
+        # make backslash readable
         $dumped =~ s/\\\\/\\/g;
+
+        # make regular expression readable (qr/\// => qr{/})
         $dumped =~ s{\b(qr/.*)}{
                 my $x = $1;
-                my ($extracted, $remainder) = extract_quotelike($x);
+                require Text::Balanced;
+                my ($extracted, $remainder) = Text::Balanced::extract_quotelike($x);
                 $extracted =~ s{\\/}{/}g;
                 $extracted =~ s{\Aqr/\Q(?^\E([^:]+):(.+)[)]/\z}{qr{$2}$1};
                 $extracted =~ s{\Aqr/(.+)/\z}{$1};
                 $extracted . $remainder;
             }eg;
-        return encode_utf8($dumped);
+        require Encode;
+        return Encode::encode_utf8($dumped);
     };
 
-    my $import = warnings->can('import');
+    # Even if warnings is loaded instead of Slack::Log, enable Smart::Comments.
+    my $import = \&warnings::import;
     *warnings::import = sub {
         Smart::Comments->import(qw(-ENV));
         goto &{$import};
     };
-    warnings->import;
+}
 
+sub import {
+    warnings->import;
     return;
 }
 

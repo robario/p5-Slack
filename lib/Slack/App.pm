96623c5772c6908f76ec7014745834e0839ddc4b
robario <webmaster@robario.com>
2013-02-07 14:02:25 +0900

    Add a minimal context object for template

--- lib/Slack/App.pm
+++ lib/Slack/App.pm
@@ -108,6 +108,7 @@ sub call {
     }
 
     my $res = Slack::Response->new;
+    $res->stash->{c} = { app => $self };    # for template
     ### assert: not defined $res->status
     ### assert: not defined $res->body
     ### assert: not length $res->content_type
